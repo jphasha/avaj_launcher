8bc0456ed61a9c27bd48021d804c9e40a9f77d75
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2020-02-12 17:21:10 +0200

    static bra. static

--- royal_air_services/Aircraft.java
+++ royal_air_services/Aircraft.java
@@ -5,7 +5,7 @@ public abstract class Aircraft {
     protected long          id;
     protected String        name;
     protected Coordinates   coordinates;
-    private long            idCounter = 0;
+    private static long            idCounter = 0;
 
     protected Aircraft(String name, Coordinates coordinates) {
         
@@ -15,6 +15,6 @@ public abstract class Aircraft {
     }
 
     private long nextId() {
-        return this.idCounter++;
+        return idCounter++;
     }
 }
