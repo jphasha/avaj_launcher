40af98d4e477c086f363e441d1ff584a55bf16a0
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2020-02-16 12:19:27 +0200

    i can read from a text file now.

--- royal_air_services/Simulator.java
+++ royal_air_services/Simulator.java
@@ -1,11 +1,17 @@
 package royal_air_services;
 
+// java.io.*; can easily solve my problems below but i chose to call these methods individually to familiarise myself with this class.
 import java.io.BufferedReader; 
 import java.io.IOException; 
 import java.io.InputStreamReader;
 
+// buffered reader above can also do it but to hell with it. what else can do it?
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Simulator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
 
         // let us start reading our standard input
         InputStreamReader myInput = new InputStreamReader(System.in);
@@ -16,7 +22,17 @@ public class Simulator {
         // read line from our buffered input. i.e. put in string format
         String inputString = myBufferedInput.readLine();
 
+        // now the read from the text file part.
+        // first define the file to be read:
+        File someTextFile = new File("/goinfre/jphasha/Desktop/thisFile.txt");
+
+        // now read it using Scanner
+        Scanner fileContent = new Scanner(someTextFile);
+
         // print the input to the standard
         System.out.println(inputString);
+        while (fileContent.hasNext()) {
+            System.out.println(fileContent.nextLine());
+        }
     }
 }
