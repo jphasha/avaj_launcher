fe29e147a1179e90b428755b300067c8d3c3c56f
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2020-02-16 14:33:05 +0200

    so far we can read and write into a text file. this will come in handy later when we read the simulation.txt

--- royal_air_services/Simulator.java
+++ royal_air_services/Simulator.java
@@ -10,6 +10,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// write into a file
+import java.io.FileWriter;
+
 public class Simulator {
     public static void main(String[] args) throws IOException, FileNotFoundException {
 
@@ -22,8 +25,32 @@ public class Simulator {
         // read line from our buffered input. i.e. put in string format
         String inputString = myBufferedInput.readLine();
 
+        // you can also create this file with code:
+        try {
+            File fileCreator = new File("/goinfre/jphasha/Desktop/createdFile.txt");
+
+            if (fileCreator.createNewFile()) {
+                System.out.println("succesfully created file");
+            } else {
+                System.out.println("file already exists");
+            }
+        } catch (IOException err) {
+            System.out.println("an error has occured");
+            err.printStackTrace();
+        }
+
+        // now that we have a file, let's write into it.
+        try {
+            FileWriter ourWriter = new FileWriter("/goinfre/jphasha/Desktop/createdFile.txt");
+            ourWriter.write("you see?\n it is very much possible to do this thing");
+            ourWriter.close();
+            System.out.println("well done, you ha written into a file");
+        } catch (IOException error) {
+            System.out.println("well! that is kapoots");
+            error.printStackTrace();
+        }
         // now the read from the text file part.
-        // first define the file to be read:
+        // define the file to be read:
         File someTextFile = new File("/goinfre/jphasha/Desktop/thisFile.txt");
 
         // now read it using Scanner
@@ -33,6 +60,14 @@ public class Simulator {
         System.out.println(inputString);
         while (fileContent.hasNext()) {
             System.out.println(fileContent.nextLine());
+        }
+
+        // delete file(s)
+        File delFile = new File("/goinfre/jphasha/Desktop/createdFile.txt");
+        if (delFile.delete()) {
+            System.out.println("that file is outta here");
+        } else {
+            System.out.println("Listen, that file is so far gone.\nIt is outta here");
         }
     }
 }
