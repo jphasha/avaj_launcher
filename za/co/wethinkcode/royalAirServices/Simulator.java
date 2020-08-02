2ba67026622cb69b9b434d5d8d652fa6e2566d7a
jphasha <jphasha@student.wethinkcode.co.za>
2020-08-03 00:36:46 +0200

    introducing methods

--- za/co/wethinkcode/royalAirServices/Simulator.java
+++ za/co/wethinkcode/royalAirServices/Simulator.java
@@ -8,28 +8,37 @@ import java.util.Scanner;
 public class Simulator {
     public static void main(String[] args) {
         if (args.length == 1) {
+            correctFormat(args);
+        } else {
+            wrongFormat(args);
+        }
+    }
+
+    private static void correctFormat(String[] args) {
+        try {
+            File scenarioFile = new File(args[0]);
+            FileWriter simulationFile = new FileWriter("simulation.txt");
+            Scanner scenarioReader = new Scanner(scenarioFile);
+
             try {
-                File scenarioFile = new File(args[0]);
-                FileWriter simulationFile = new FileWriter("simulation.txt");
-                Scanner scenarioReader = new Scanner(scenarioFile);
-
-                try {
-                    simulationFile.write("Something");
-                    simulationFile.close();
-                    System.out.println("file created and written into successfully");
-                } catch (IOException err) {
-                    err.getMessage();
-                }
-
-                while (scenarioReader.hasNextLine()) {
-                    String scenarioText = scenarioReader.nextLine();
-                    System.out.println(scenarioText);
-                }
-                scenarioReader.close();
-            } catch (IOException exc) {
-                System.out.println(exc.getMessage());
+                simulationFile.write("Something");
+                simulationFile.close();
+                System.out.println("file created and written into successfully");
+            } catch (IOException err) {
+                err.getMessage();
             }
-        } else if (args.length > 1) {
+
+            while (scenarioReader.hasNextLine()) {
+                String scenarioText = scenarioReader.nextLine();
+                System.out.println(scenarioText);
+            }
+            scenarioReader.close();
+        } catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+    }
+    private static void wrongFormat(String[] args) {
+        if (args.length > 1) {
             System.out.println("this program only accepts one argument, Prefeably a file named 'scenario.txt'");
         } else {
             System.out.println("you need to have 1 argument when running this program\nPreferrably a file named'scenario.txt'.");
