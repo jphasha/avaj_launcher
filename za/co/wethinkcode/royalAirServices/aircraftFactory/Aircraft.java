498835cd4f2d7987a0eb2efdaeabf803c8674b16
jphasha <jphasha@student.wethinkcode.co.za>
2020-08-17 17:51:10 +0200

    When instantiated, assign name, coordinates and id(unique)

---
+++ za/co/wethinkcode/royalAirServices/aircraftFactory/Aircraft.java
@@ -0,0 +1,22 @@
+package za.co.wethinkcode.royalAirServices.aircraftFactory;
+
+public class Aircraft {
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+    private static long idCounter;
+
+    static {
+        idCounter = 0;
+    }
+
+    protected Aircraft(String name, Coordinates coordinates) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.id = nextId();
+    }
+    private long nextId() {
+        this.id = ++idCounter;
+        return this.id;
+    }
+}
