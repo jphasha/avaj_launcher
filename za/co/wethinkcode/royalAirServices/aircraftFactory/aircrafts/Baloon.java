ebcabac1c588eac9c6133ce645c0eda5ff16f119
jphasha <jphasha@student.wethinkcode.co.za>
2020-08-17 18:31:04 +0200

    skeleton aircrafts, they inherit Aircraft hence super in constructor and implements interface Flyable via updateConditions() and registerTower() although implementation will come later, let me fix other classes first

---
+++ za/co/wethinkcode/royalAirServices/aircraftFactory/aircrafts/Baloon.java
@@ -0,0 +1,17 @@
+package za.co.wethinkcode.royalAirServices.aircraftFactory.aircrafts;
+
+import za.co.wethinkcode.royalAirServices.aircraftFactory.Aircraft;
+import za.co.wethinkcode.royalAirServices.aircraftFactory.Coordinates;
+import za.co.wethinkcode.royalAirServices.interfaces.Flyable;
+import za.co.wethinkcode.royalAirServices.tower.weather.WeatherTower;
+
+public class Baloon extends Aircraft implements Flyable {
+    private WeatherTower weatherTower;
+
+    public Baloon(String name, Coordinates coordinates) {
+        super(name, coordinates);
+    }
+
+    public void updateConditions() {}
+    public void registerTower(WeatherTower weatherTower) {}
+}
