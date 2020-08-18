5bab9be57e147914f9f0d005fc2eb4c657376d1b
jphasha <jphasha@student.wethinkcode.co.za>
2020-08-18 14:25:45 +0200

    i forgot that the constructor is private, use getProvider() instead

--- za/co/wethinkcode/royalAirServices/tower/weather/WeatherProvider.java
+++ za/co/wethinkcode/royalAirServices/tower/weather/WeatherProvider.java
@@ -6,7 +6,7 @@ public class WeatherProvider {
     private static WeatherProvider weatherProvider = new WeatherProvider();
     private static String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
 
-    public WeatherProvider() {}
+    private WeatherProvider() {}
 
     public static WeatherProvider getProvider() {
         return weatherProvider;
