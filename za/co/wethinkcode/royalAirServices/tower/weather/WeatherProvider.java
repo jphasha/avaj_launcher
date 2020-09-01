0faf43f9ffd592e4ed465405676125d1906d5a76
jphasha <jphasha@student.wethinkcode.co.za>
2020-09-02 00:10:55 +0200

    I am drawing the line here, will perfect later

--- za/co/wethinkcode/royalAirServices/tower/weather/WeatherProvider.java
+++ za/co/wethinkcode/royalAirServices/tower/weather/WeatherProvider.java
@@ -13,22 +13,11 @@ public class WeatherProvider {
     }
 
     public String getCurrentWeather(Coordinates coordinates) {
-        int heightWeatherCoordinate = coordinates.getHeight() % 4;
-        int latiWeatherCoordinate = (1 + coordinates.getLatitude()) % 4;
-        int longiWeatherCoordinate = (2 + coordinates.getLongitude()) % 4;
+        int coordinateSum = coordinates.getHeight() + coordinates.getLatitude() + coordinates.getLongitude();
+        int weatherIndex = coordinateSum % 4;
         String weatherConditions = null;
 
-        if ((heightWeatherCoordinate == latiWeatherCoordinate) || (heightWeatherCoordinate == longiWeatherCoordinate)) {
-            weatherConditions = weather[heightWeatherCoordinate];
-        } else if ((latiWeatherCoordinate == longiWeatherCoordinate)) {
-            weatherConditions = weather[latiWeatherCoordinate];
-        } else {
-            for(int i = 0; i < 4; i++) {
-                if ((i != heightWeatherCoordinate) && (i != latiWeatherCoordinate) && (i != longiWeatherCoordinate)) {
-                    weatherConditions = weather[i];
-                }
-            }
-        }
+        weatherConditions = weather[weatherIndex];
         return weatherConditions;
     }
 }
